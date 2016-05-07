7f56405e6ca64fbdff2e62f14e8eed8df30462bb
JiaMingLin <robin890650@gmail.com>
2016-05-07 12:03:22 +0800

    modify the zeppelin setup process

--- spark/images/zeppelin/docker-zeppelin.sh
+++ spark/images/zeppelin/docker-zeppelin.sh
@@ -18,4 +18,4 @@ export ZEPPELIN_HOME=/opt/zeppelin
 export ZEPPELIN_CONF_DIR="${ZEPPELIN_HOME}/conf"
 
 echo "=== Launching Zeppelin under Docker ==="
-/opt/zeppelin/bin/zeppelin.sh "${ZEPPELIN_CONF_DIR}"
+/opt/zeppelin/bin/zeppelin.sh
--- spark/images/zeppelin/zeppelin-env.sh
+++ spark/images/zeppelin/zeppelin-env.sh
@@ -14,13 +14,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-export MASTER="spark://spark-master-svc:7077"
-export SPARK_HOME=/opt/spark
-export ZEPPELIN_JAVA_OPTS="-Dspark.jars=/opt/spark/lib/gcs-connector-latest-hadoop2.jar"
+#export MASTER="spark://spark-master-svc:7077"
+#export SPARK_HOME=/opt/spark
+#export ZEPPELIN_JAVA_OPTS="-Dspark.jars=/opt/spark/lib/gcs-connector-latest-hadoop2.jar"
 # TODO(zmerlynn): Setting global CLASSPATH *should* be unnecessary,
 # but ZEPPELIN_JAVA_OPTS isn't enough here. :(
 # export CLASSPATH="/opt/spark/lib/gcs-connector-latest-hadoop2.jar"
 export ZEPPELIN_NOTEBOOK_DIR="${ZEPPELIN_HOME}/notebook"
 export ZEPPELIN_MEM=-Xmx1024m
 export ZEPPELIN_PORT=8080
-export PYTHONPATH="${SPARK_HOME}/python:${SPARK_HOME}/python/lib/py4j-0.8.2.1-src.zip"
+#export PYTHONPATH="${SPARK_HOME}/python:${SPARK_HOME}/python/lib/py4j-0.8.2.1-src.zip"
